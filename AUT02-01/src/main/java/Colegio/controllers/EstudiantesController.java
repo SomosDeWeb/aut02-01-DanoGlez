966b12e4ace4fe137413951e9769360bcd3fd847
Daniel Darias <fdariasglez@hotmail.com>
2025-10-03 14:15:56 +0100

    Buscar un estudiante por su nombre

--- AUT02-01/src/main/java/Colegio/controllers/EstudiantesController.java
+++ AUT02-01/src/main/java/Colegio/controllers/EstudiantesController.java
@@ -26,4 +26,19 @@ public class EstudiantesController {
             System.out.println(a.getNombre() + " con Edad " + a.getEdad() + " tiene " + a.getNota() + " de nota media.");
         }
     }
+
+    public static void buscarEstudiante(){
+        String Resultado = "No encontrado";
+        String Nombre = Reproductor("¿Cuál es el nombre del Estudiante que busca? ");
+
+        for (Estudiante a : listaEstudiantes){
+            if(Objects.equals(a.getNombre(), Nombre)){
+                Resultado = (a.getNombre() + " con Edad " + a.getEdad() + " tiene " + a.getNota() + " de nota media.");
+                break;
+            }
+        }
+
+        System.out.println("Resultado de búsqueda: " + Resultado);
+
+    }
 }
